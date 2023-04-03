dbf72a4e186c70f3c13e82ecbe89bd196b4987da
vdomobv <vdomobv@naver.com>
2023-04-03 17:58:33 +0900

    23.04.03

---
+++ algorithm/23.04.03/swea_연산.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+def solve(n, cnt):
+    global ans
+
+
+    
+
+for tc in range(1, t+1):
+    n, m = map(int, input().split())
+    ans = 99999999
+    solve(n, 0)
+    print(f"#{tc} {cnt}")
