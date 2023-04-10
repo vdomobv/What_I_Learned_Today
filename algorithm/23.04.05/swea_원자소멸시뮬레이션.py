d419631a3eaa1a76eee35d9140c4f0b7f01804d1
vdomobv <vdomobv@naver.com>
2023-04-10 17:25:52 +0900

    23.04.10

--- algorithm/23.04.05/swea_원자소멸시뮬레이션.py
+++ algorithm/23.04.05/swea_원자소멸시뮬레이션.py
@@ -1,24 +1,39 @@
-t = int(input())
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for tc in range(1, t+1):
+T = int(input())
+for tc in range(1, 1+T):
     n = int(input())
+    atom = [list(map(int, input().split())) for _ in range(n)]
+    # 격자 중간에서 만날 수도 있기 때문에 0.5초 단위로 이동한다고 가정했음
+    d = [(0, 0.5), (0, -0.5), (-0.5, 0), (0.5, 0)]
 
-    atom = []
-    for _ in range(n):
-        x, y, d, k = map(int, input().split())
-        atom.append([x, y, d, k])
-    
-"""
-접근 방식
-1. 원자 리스트를 차례로 돌면서 진행방향으로 한칸씩 옮겨준다.
-2. 이동 시에 1초에 1칸씩 이동하지만,
-    두 원자 사이칸이 홀수인 경우 .5초만에 만나므로
-    0.5초를 고려하여 이동해야 함.
-2. 원자들을 다 옮겼을때 같은 좌표에 도달하는 원자들을 소멸 시킨다.
-3. 종료조건? 무한히 이동하는 점이 있는 경우 어떻게 하지?? - 범위 바깥일 경우 종료
+    result = 0
 
-"""
+    # 아톰 개수가 2개 이하가 되면 만날 아톰이 없으니까 종료
+    while len(atom) >= 2:
+        # 모든 아톰을 0.5초 단위로 이동시킨다
+        for i in range(len(atom)):
+            atom[i][0] = atom[i][0] + d[atom[i][2]][0]
+            atom[i][1] = atom[i][1] + d[atom[i][2]][1]
 
+        # 좌표를 딕셔너리로 표현
+        location = {}
+        # 각 아톰들을 순회하면서 좌표: [아톰들]의 형태로 넣어준다.
+        for a in atom:
+            try:
+                location[(a[0], a[1])].append(a)
+            except Exception:
+                location[(a[0], a[1])] = [a]
+
+        # 아톰 리스트를 초기화하고
+        atom = []
+        for l in location:
+
+            if len(location[l]) >= 2:
+                # 만약 같은 위치에 아톰이 여러개라면 결과값에 아톰의 에너지들을 결과값에 넣어주고
+                for at in location[l]:
+                    result += at[3]
+            else:
+                # 위치에 아톰이 1개라면 범위내에 있는 아톰들은 아톰 리스트에 다시 넣어준다.
+                if -1000 <= location[l][0][0] <= 1000 and -1000 <= location[l][0][1] <= 1000:
+                    atom.append(location[l][0])
+
+    print(f'#{tc}', result)
