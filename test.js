03f7a18a0e505f0347903f14bb0e06a34c9cf150
eazsilver <vdomobv@naver.com>
2023-04-29 21:44:55 +0900

    23.04.29

---
+++ test.js
@@ -0,0 +1,29 @@
+console.log(x)
+var x = 10
+
+const word2=`안녕
+하세요`
+console.log(word2)
+
+const userInformation = {
+    name : 'ssafy kim',
+    userId : 'ssafyStudent1234',
+    email : 'ssafy@ssafy.com'
+}
+
+const name = userInformation.name
+const {userId, email} = userInformation
+console.log(name)
+console.log(userId, email)
+
+console.log('hi')
+setTimeout(function ssafy() {
+    console.log('ssafy')
+}, 0);
+console.log('bye')
+console.log('hiagain')
+console.log('byeagain')
+console.log('hiagain')
+console.log('byeagain')
+console.log('hiagain')
+console.log('byeagain')
